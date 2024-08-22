fe439d4ca9fe0a18ac4fea6e143e95aa5f10b3f1
ayushisingh0894 <a4ayushi0894@gmail.com>
2024-08-22 14:26:52 +0530

    Data Ingestion

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -9,7 +9,7 @@ from sklearn.model_selection import train_test_split
 @dataclass
 class DataIngestionConfig:
     train_data_path: str=os.path.join('artifacts','train.csv')
-    test_data_path: str=os.path.join('artifacts','train.csv')
+    test_data_path: str=os.path.join('artifacts','test.csv')
     raw_data_path: str=os.path.join('artifacts','raw.csv')
 
 class DataIngestion:
@@ -19,16 +19,16 @@ class DataIngestion:
     def initiate_data_ingestion(self):
             logging.info("Entered the initiate_data_ingestion")
             try:
-                df=pd.read_csv('notebook\data\stud.csv')
-                logging.info('Data is read')
+                df=pd.read_csv('notebook/data/stud.csv')
+                logging.info('Read the dataset as dataframe')
 
                 os.makedirs(os.path.dirname((self.ingestion_config.train_data_path)),exist_ok=True)
                 df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
                 logging.info('Folders for data is created')
 
                 train_set,test_set= train_test_split(df,test_size=0.2,random_state=40)
-                train_set.to_csv(self.ingestion_config.train_data_path_data_path,index=False,header=True)
-                test_set.to_csv(self.ingestion_config.test_data_path_data_path,index=False,header=True)
+                train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+                test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
 
                 logging.info('Data is now train test split')
 
